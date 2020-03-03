2de715da131a1d34b6aa21cc95490e7301bf34c6
Sud <363819252@qq.com>
2020-03-03 17:33:37 +0800

    frist

---
+++ git文档/git  警告提示（LF will be replaced by CRLF in readme.md）处理方法.txt
@@ -0,0 +1,5 @@
+win:
+git config --global core.autocrlf true
+
+LinuxªÚMacœµÕ≥:
+git config --global core.autocrlf input
---
+++ vue-cli 4项目中使用mockjs模拟数据 教程/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import axios from 'axios' // axios http请求库
+
+axios.defaults.baseURL = 'http://mockjs.com/api' // 设置默认请求的url
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+process.env.NODE_ENV === 'development' && require('../mock/mock.js');// 开发环境引入mock
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
